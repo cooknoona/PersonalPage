659a6ee0506576be216ace21d3e2f8474df328a4
Cook <cook961004@gmail.com>
2024-11-05 17:41:33 +0900

    Update for some CSS

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const languageResource = {
+  ko: {
+    cate1: "정보",
+    cate2: "프로젝트",
+    cate3: "포트폴리오",
+    cate4: "연락처",
+  },
+  en: {
+    cate1: "About",
+    cate2: "PROJECT",
+    cate3: "PORTFOLIO",
+    cate4: "CONTACT",
+  },
+};
+
+window.addEventListener("load", function () {
+  this.document.getElementById()
+});
